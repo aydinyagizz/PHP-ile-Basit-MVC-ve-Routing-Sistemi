8c53924d7dc09f5d09013c41998a155c45bb0fce
Aydın Yağız <aydinyagiz002@gmail.com>
2021-08-05 23:44:38 +0300

    Add files via upload

---
+++ PHP ile Basit MVC ve Routing Sistemi/controller.php
@@ -0,0 +1,25 @@
+<?php 	
+
+
+class Controller 
+{
+	public function view($name, $data = [])
+	{
+
+		extract($data); //data gönderirse.
+		require __DIR__ . '/view/' . strtolower($name) . '.php';
+	}
+
+	public function model($name)
+	{
+		require __DIR__ . '/model/' . strtolower($name) . '.php';
+		return new $name();
+	}
+}
+
+
+
+
+
+
+ ?>
